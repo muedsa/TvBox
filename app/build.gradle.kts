4cfb4023e35115090d9a5e3af3d8d7595ee3de5f
MUEDSA <7676275+muedsa@users.noreply.github.com>
2024-10-15 20:03:34 +0800

    build: release签名

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -6,6 +9,12 @@ plugins {
     alias(libs.plugins.hilt.android)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.android.room)
+}
+
+val keystorePropertiesFile: File = rootProject.file("keystore.properties")
+val keystoreProperties = Properties()
+if (keystorePropertiesFile.exists() && keystorePropertiesFile.canRead()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
 android {
@@ -21,6 +30,23 @@ android {
 
     }
 
+    signingConfigs {
+        create("release") {
+            if (keystoreProperties.containsKey("muedsa.signingConfig.storeFile")) {
+                storeFile = file(keystoreProperties["muedsa.signingConfig.storeFile"] as String)
+                storePassword = keystoreProperties["muedsa.signingConfig.storePassword"] as String
+                keyAlias = keystoreProperties["muedsa.signingConfig.keyAlias"] as String
+                keyPassword = keystoreProperties["muedsa.signingConfig.keyPassword"] as String
+            } else {
+                val debugSigningConfig = signingConfigs.getByName("debug")
+                storeFile = debugSigningConfig.storeFile
+                storePassword = debugSigningConfig.storePassword
+                keyAlias = debugSigningConfig.keyAlias
+                keyPassword = debugSigningConfig.keyPassword
+            }
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -28,12 +54,15 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("release")
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
