d94159eca05b910e3a2ef84416e923deb555fdb8
MUEDSA <7676275+muedsa@users.noreply.github.com>
2024-11-01 14:55:48 +0800

    feat: 播放器背景改为纯黑色

--- app/src/main/java/com/muedsa/tvbox/screens/playback/PlaybackWidget.kt
+++ app/src/main/java/com/muedsa/tvbox/screens/playback/PlaybackWidget.kt
@@ -1,6 +1,7 @@
 package com.muedsa.tvbox.screens.playback
 
 import androidx.annotation.OptIn
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -8,6 +9,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.media3.common.MediaItem
 import androidx.media3.common.MimeTypes
@@ -18,6 +21,9 @@ import androidx.media3.datasource.DefaultDataSource
 import androidx.media3.datasource.DefaultHttpDataSource
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.exoplayer.source.DefaultMediaSourceFactory
+import androidx.tv.material3.MaterialTheme
+import androidx.tv.material3.Surface
+import androidx.tv.material3.SurfaceDefaults
 import com.kuaishou.akdanmaku.data.DanmakuItemData
 import com.kuaishou.akdanmaku.ecs.component.filter.DuplicateMergedFilter
 import com.muedsa.compose.tv.useLocalNavHostController
@@ -83,91 +89,100 @@ fun PlaybackWidget(
         }
     }
     val androidContext = LocalContext.current
-    DanmakuVideoPlayer(
-        danmakuConfigSetting = {
-            textSizeScale = appSetting.danmakuSizeScale / 100f
-            alpha = appSetting.danmakuAlpha / 100f
-            screenPart = appSetting.danmakuScreenPart / 100f
-            dataFilter = listOf(DuplicateMergedFilter().apply { enable = true })
-        },
-        danmakuPlayerInit = {
-            if (danmakuList.isNotEmpty()) {
-                var list = danmakuList
-                if (appSetting.danmakuMergeEnable) {
-                    list = list.mergeDanmaku(5000L, 60000L, 30)
+    Surface(
+        modifier = Modifier
+            .fillMaxSize(),
+        colors = SurfaceDefaults.colors(
+            containerColor = Color.Black,
+            contentColor = MaterialTheme.colorScheme.onBackground
+        )
+    ) {
+        DanmakuVideoPlayer(
+            danmakuConfigSetting = {
+                textSizeScale = appSetting.danmakuSizeScale / 100f
+                alpha = appSetting.danmakuAlpha / 100f
+                screenPart = appSetting.danmakuScreenPart / 100f
+                dataFilter = listOf(DuplicateMergedFilter().apply { enable = true })
+            },
+            danmakuPlayerInit = {
+                if (danmakuList.isNotEmpty()) {
+                    var list = danmakuList
+                    if (appSetting.danmakuMergeEnable) {
+                        list = list.mergeDanmaku(5000L, 60000L, 30)
+                    }
+                    updateData(list)
                 }
-                updateData(list)
-            }
-        },
-        videoPlayerBuilderSetting = {
-            if (!httpHeaders.isNullOrEmpty()) {
-                setMediaSourceFactory(
-                    DefaultMediaSourceFactory(
-                        DefaultDataSource.Factory(androidContext,
-                            DefaultHttpDataSource.Factory().apply {
-                                setDefaultRequestProperties(httpHeaders)
-                            }
+            },
+            videoPlayerBuilderSetting = {
+                if (!httpHeaders.isNullOrEmpty()) {
+                    setMediaSourceFactory(
+                        DefaultMediaSourceFactory(
+                            DefaultDataSource.Factory(androidContext,
+                                DefaultHttpDataSource.Factory().apply {
+                                    setDefaultRequestProperties(httpHeaders)
+                                }
+                            )
                         )
                     )
-                )
-            }
-        }
-    ) {
-        addListener(object : Player.Listener {
-
-            override fun onPlayerErrorChanged(error: PlaybackException?) {
-                toastController.error(error, SnackbarDuration.Long)
-                error?.let {
-                    Timber.i(it, "exoplayer mediaUrl: $url")
                 }
             }
+        ) {
+            addListener(object : Player.Listener {
 
-            override fun onRenderedFirstFrame() {
-                if (exoplayerHolder == null) {
-                    exoplayerHolder = this@DanmakuVideoPlayer
-                    if (episodeProgress.progress > 0) {
-                        val position =
-                            if (episodeProgress.progress == episodeProgress.duration) {
-                                // 如果上次已经播放完成则不跳转 从头播放
-                                0
-                            } else if (episodeProgress.duration > 5_000 && episodeProgress.progress > episodeProgress.duration - 5_000) {
-                                // 如果太过接近结束的位置
-                                episodeProgress.duration - 5_000
-                            } else {
-                                episodeProgress.progress
-                            }
-                        if (position > 0) {
-                            seekTo(position)
-                            val positionStr = position
-                                .toDuration(DurationUnit.MILLISECONDS)
-                                .toComponents { hours, minutes, seconds, _ ->
-                                    String.format(
-                                        locale = java.util.Locale.getDefault(),
-                                        "%02d:%02d:%02d",
-                                        hours,
-                                        minutes,
-                                        seconds,
-                                    )
+                override fun onPlayerErrorChanged(error: PlaybackException?) {
+                    toastController.error(error, SnackbarDuration.Long)
+                    error?.let {
+                        Timber.i(it, "exoplayer mediaUrl: $url")
+                    }
+                }
+
+                override fun onRenderedFirstFrame() {
+                    if (exoplayerHolder == null) {
+                        exoplayerHolder = this@DanmakuVideoPlayer
+                        if (episodeProgress.progress > 0) {
+                            val position =
+                                if (episodeProgress.progress == episodeProgress.duration) {
+                                    // 如果上次已经播放完成则不跳转 从头播放
+                                    0
+                                } else if (episodeProgress.duration > 5_000 && episodeProgress.progress > episodeProgress.duration - 5_000) {
+                                    // 如果太过接近结束的位置
+                                    episodeProgress.duration - 5_000
+                                } else {
+                                    episodeProgress.progress
                                 }
-                            toastController.info("跳转到上次播放位置: $positionStr")
+                            if (position > 0) {
+                                seekTo(position)
+                                val positionStr = position
+                                    .toDuration(DurationUnit.MILLISECONDS)
+                                    .toComponents { hours, minutes, seconds, _ ->
+                                        String.format(
+                                            locale = java.util.Locale.getDefault(),
+                                            "%02d:%02d:%02d",
+                                            hours,
+                                            minutes,
+                                            seconds,
+                                        )
+                                    }
+                                toastController.info("跳转到上次播放位置: $positionStr")
+                            }
                         }
                     }
                 }
-            }
 
-            override fun onPlaybackStateChanged(playbackState: Int) {
-                val ended = playbackState == Player.STATE_ENDED
-                if (playerEnd != ended) {
-                    playerEnd = ended
+                override fun onPlaybackStateChanged(playbackState: Int) {
+                    val ended = playbackState == Player.STATE_ENDED
+                    if (playerEnd != ended) {
+                        playerEnd = ended
+                    }
                 }
+            })
+            playWhenReady = true
+            val mediaItemBuilder = MediaItem.Builder().setUri(url)
+            if (url.endsWith(".m3u8", ignoreCase = true)) {
+                mediaItemBuilder.setMimeType(MimeTypes.APPLICATION_M3U8)
             }
-        })
-        playWhenReady = true
-        val mediaItemBuilder = MediaItem.Builder().setUri(url)
-        if (url.endsWith(".m3u8", ignoreCase = true)) {
-            mediaItemBuilder.setMimeType(MimeTypes.APPLICATION_M3U8)
+            setMediaItem(mediaItemBuilder.build())
+            prepare()
         }
-        setMediaItem(mediaItemBuilder.build())
-        prepare()
     }
 }
